ab53770a6ef2863136f47e2d73087edeacb85e97
sodalitesoloution <crazymian44@gmail.com>
2016-12-24 19:23:14 +0500

    Add files via upload

---
+++ Category.java
@@ -0,0 +1,33 @@
+package yasir.pakcelebrationwallpaper;
+
+/**
+ * Created by yasir on 23/12/2016.
+ */
+
+public class Category {
+    public String id, title;
+
+    public Category() {
+    }
+
+    public Category(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
